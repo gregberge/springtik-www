35dbff1c823b9dfd75080991f3eb5220b859323e
Greg Bergé <neoziro@gmail.com>
2016-03-02 23:16:38 +0100

    chore(api): unactive api

--- src/server/index.js
+++ src/server/index.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import http from 'http';
 import config from '../config';
-// import subdomain from 'express-subdomain';
-import api from './api';
-// import admin from './admin';
-// import www from './www';
+import subdomain from 'express-subdomain';
+// import api from './api';
+import admin from './admin';
+import www from './www';
 import compress from 'compression';
 
 const app = express();
 
 app.use(compress());
 
-app.use('/api', api);
-// app.use(subdomain('admin', admin));
-// app.use(subdomain('www', www));
+// app.use('/api', api);
+app.use(subdomain('admin', admin));
+app.use(subdomain('www', www));
 
 const server = http.createServer(app);
 server.listen(config.get('server.port'));
