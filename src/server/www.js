066e217f6e15483dcdcfe29ffc826ff21fadecdc
Greg Bergé <neoziro@gmail.com>
2016-03-13 23:29:19 +0100

    fix(www): fix public path

--- src/server/www.js
+++ src/server/www.js
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 
 const router = express.Router();
-const publicPath = path.join(__dirname, '../../../public');
+const publicPath = path.join(__dirname, '../../public');
 
 router.use(express.static(path.join(publicPath, 'www')));
 
