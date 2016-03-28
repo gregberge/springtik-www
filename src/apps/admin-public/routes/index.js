dac71d3a2e8db9a7624beda0991bf0d313fe9571
Greg Bergé <neoziro@gmail.com>
2016-03-28 11:08:07 +0200

    feat(login): redirect all urls to login

--- src/apps/admin-public/routes/index.js
+++ src/apps/admin-public/routes/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Route from 'react-router/lib/Route';
-import IndexRedirect from 'react-router/lib/IndexRedirect';
+import Redirect from 'react-router/lib/Redirect';
 import App from './App';
 import Login from './login/Login';
 
 export default [
   <Route path="/" component={App}>
     <Route path="login" component={Login} />
-    <IndexRedirect to="login" />
+    <Redirect from="*" to="login" />
   </Route>
 ];
