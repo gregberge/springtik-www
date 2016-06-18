d5f1b29d5297f5c6ab06fcfd6005535f6a282fb2
Greg Bergé <neoziro@gmail.com>
2016-06-18 16:58:08 +0200

    feat(home): use gradient for progressie loading

--- src/apps/www-public/routes/home/HomeBackground.js
+++ src/apps/www-public/routes/home/HomeBackground.js
@@ -5,7 +5,9 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './HomeBackground.scss';
 
 export const HomeBackground = () => (
-  <div className={styles.background} />
+  <div className={styles.background}>
+    <div className={styles.image} />
+  </div>
 );
 
 export default compose(
